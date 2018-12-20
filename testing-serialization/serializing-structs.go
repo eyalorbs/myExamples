e31d04286ff841404e9415eb06d4881fb32353c0
eyal <eyals@orbs.com>
2018-12-20 02:14:24 +0200

    "started to work on tictactoe and to work on serialization of the structs"

--- testing-serialization/serializing-structs.go
+++ testing-serialization/serializing-structs.go
@@ -2,49 +2,97 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"log"
 )
+
+func (board *GameBoard) MarshalJSON() (b []byte, err error) {
+	if len(*board) != 9 {
+		panic("invalid board size")
+	}
+	var s string
+	s = string(*board)
+	return json.Marshal(s)
+}
+
+func (board *GameBoard) UnmarshalJSON(b []byte) (err error) {
+	var s string
+	err = json.Unmarshal(b, &s)
+	if err != nil {
+		return err
+	}
+	for i := 0; i < 9; i++ {
+		*board = append(*board, rune(s[i]))
+	}
+
+	return nil
+}
+
+func (game *Game) MarshalJSON() (b []byte, err error) {
+	mappedGame := make(map[string][]byte)
+
+	//write the board value to the map
+	mappedGame["board"], err = game.Board.MarshalJSON()
+	if err != nil {
+		return []byte{}, err
+	}
+
+	//write the player's to the map
+	mappedGame["playerX"] = game.PlayerX
+	mappedGame["playerO"] = game.PlayerO
+
+	//write the player's turn to the map
+	if game.PlayerXTurn {
+		mappedGame["playerXTurn"] = []byte{1}
+	} else {
+		mappedGame["playerXTurn"] = []byte{0}
+	}
+
+	//return the marshaled map
+	return json.Marshal(mappedGame)
+}
+
+func (game *Game) UnmarshalJSON(b []byte) (err error) {
+	//unmarshal the data into a map
+	mappedGame := make(map[string][]byte)
+	err = json.Unmarshal(b, &mappedGame)
+	if err != nil {
+		panic(err)
+	}
+
+	boardBytes := mappedGame["board"]
+	var board GameBoard
+	err = board.UnmarshalJSON(boardBytes)
+	if err != nil {
+		return err
+	}
+	game.Board = board
+	game.PlayerX = mappedGame["playerX*"]
+	game.PlayerO = mappedGame["playerO"]
+
+	if mappedGame["playerXTurn"][0] == 1 {
+		game.PlayerXTurn = true
+	} else {
+		game.PlayerXTurn = false
+	}
+	return nil
+}
 
 func main() {
 
-	//create and empty board
+	//emptyBoard := GameBoard{'-', '-', '-', '-', '-', '-', '-', '-', '-'}
 
-	var board Board
-	for i := 0; i < 9; i++ {
-		board[i] = '-'
-	}
-	for _, val := range board {
-		fmt.Print(string(val))
-	}
-	fmt.Println("")
+	/*
+		game1 := Game{emptyBoard, []byte{3,5,2,5}, []byte{4,2,6,240}, true}
+		game2 := Game{emptyBoard, []byte{3,5,2,5,254}, []byte{4,2,6,240},  false}
+	*/
 
-	b, err := json.Marshal(board)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(b)
-
-	var unmarshaledBoard Board
-
-	err = json.Unmarshal(b, &unmarshaledBoard)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, val := range unmarshaledBoard {
-		fmt.Print(string(val))
-	}
-	fmt.Println("")
-	unmarshaledBoard.set(3)
-	for _, val := range unmarshaledBoard {
-		fmt.Print(string(val))
-	}
 }
 
-type Board [9]rune
+type GameBoard []rune
 
-func (board *Board) set(index uint8) {
-	board[index] = 'j'
+type Game struct {
+	Board       GameBoard `json:"board"`
+	PlayerX     []byte    `json:"playerX"`
+	PlayerO     []byte    `json:"playerO"`
+	PlayerXTurn bool      `json:"playerXTurn"`
 }
+type Games []Game
--- tictactoe/tictactoe.go
+++ tictactoe/tictactoe.go
@@ -4,24 +4,57 @@ import (
 	"bytes"
 	"github.com/orbs-network/orbs-contract-sdk/go/sdk"
 	"github.com/orbs-network/orbs-contract-sdk/go/sdk/address"
+	"github.com/orbs-network/orbs-contract-sdk/go/sdk/state"
 )
 
 var PUBLIC = sdk.Export()
 var SYSTEM = sdk.Export(_init)
 
 func _init() {
-
+	state.WriteBytesByKey("waitingPool", []byte{})
+	state.WriteBytesByKey("games", []byte{})
 }
 
-func (game *Game) play(index uint32) (feedback string) {
+func startGame() {
+	callerAddress := address.GetCallerAddress()
+
+	if len(state.ReadBytesByKey("waitingPool")) == 0 {
+		state.WriteBytesByKey("waitingPool", callerAddress)
+	} else {
+
+		state.WriteUint32ByAddress(callerAddress)
+
+	}
+}
+func (game *Game) play(index uint32) {
+
+	callerAddress := address.GetCallerAddress()
+	if !(bytes.Equal(callerAddress, game.PlayerX) || bytes.Equal(callerAddress, game.PlayerO)) {
+		panic("you are not registered as a player in this game")
+	}
+
 	if game.PlayerXTurn {
-		if bytes.Equal(address.GetCallerAddress(), game.PlayerY) {
+		if bytes.Equal(callerAddress, game.PlayerO) {
 			panic("it is player X's turn, wait for your turn")
 		}
 	} else {
-		if bytes.Equal(address.GetCallerAddress(), game.PlayerX) {
+		if bytes.Equal(callerAddress, game.PlayerX) {
 			panic("it is player Y's turn, wait for your turn")
 		}
+	}
+
+	if 8 < index {
+		panic("the index must be between 0 and 8")
+	}
+
+	if game.Board[index] != '-' {
+		panic("that index is taken")
+	}
+
+	if game.PlayerXTurn {
+		game.Board[index] = 'X'
+	} else {
+		game.Board[index] = 'Y'
 	}
 
 }
@@ -31,7 +64,7 @@ type Board [9]rune
 type Game struct {
 	Board       Board
 	PlayerX     []byte
-	PlayerY     []byte
+	PlayerO     []byte
 	PlayerXTurn bool
 }
 type Games []Game
