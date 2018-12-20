ac0f0adc0ea921b95acfba1bd756e37bab701b2c
eyal <eyals@orbs.com>
2018-12-21 01:09:58 +0200

    "testing serialization"

--- testing-serialization/serializing-structs.go
+++ testing-serialization/serializing-structs.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/binary"
 	"encoding/json"
+	"fmt"
 )
 
 func (board *GameBoard) MarshalJSON() (b []byte, err error) {
@@ -65,7 +67,7 @@ func (game *Game) UnmarshalJSON(b []byte) (err error) {
 		return err
 	}
 	game.Board = board
-	game.PlayerX = mappedGame["playerX*"]
+	game.PlayerX = mappedGame["playerX"]
 	game.PlayerO = mappedGame["playerO"]
 
 	if mappedGame["playerXTurn"][0] == 1 {
@@ -76,15 +78,71 @@ func (game *Game) UnmarshalJSON(b []byte) (err error) {
 	return nil
 }
 
+func uint64ToBytes(num uint64) (b []byte) {
+	b = make([]byte, 8)
+	binary.LittleEndian.PutUint64(b, num)
+	return b
+}
+
+func bytesToUint64(b []byte) (num uint64) {
+
+	return binary.LittleEndian.Uint64(b)
+
+}
+
+func (games *Games) MarshalJSON() (b []byte, err error) {
+	gamesMap := make(map[int][]byte)
+	//the key '0' is the length of the array
+	gamesMap[0] = uint64ToBytes(uint64(len(*games)))
+	for i := 1; i <= len(*games); i++ {
+		gamesMap[i], err = (*games)[i-1].MarshalJSON()
+		if err != nil {
+			return []byte{}, err
+		}
+	}
+	return json.Marshal(gamesMap)
+
+}
+
+func (games *Games) UnmarshalJSON(b []byte) (err error) {
+	var gamesMap map[int][]byte
+	err = json.Unmarshal(b, &gamesMap)
+	if err != nil {
+		return err
+	}
+	var tempGame Game
+	length := bytesToUint64(gamesMap[0])
+	for i := 1; i <= int(length); i++ {
+		err = tempGame.UnmarshalJSON(gamesMap[i])
+		if err != nil {
+			return err
+		}
+		*games = append(*games, tempGame)
+	}
+
+	return nil
+}
+
 func main() {
 
-	//emptyBoard := GameBoard{'-', '-', '-', '-', '-', '-', '-', '-', '-'}
+	emptyBoard1 := GameBoard{'-', '-', '-', '-', '-', '-', '-', '-', '-'}
+	emptyBoard2 := GameBoard{'-', '-', '-', '-', '-', '-', '-', '-', '-'}
 
-	/*
-		game1 := Game{emptyBoard, []byte{3,5,2,5}, []byte{4,2,6,240}, true}
-		game2 := Game{emptyBoard, []byte{3,5,2,5,254}, []byte{4,2,6,240},  false}
-	*/
+	game1 := Game{emptyBoard1, []byte{3, 5, 2, 5}, []byte{4, 2, 6, 240}, true}
+	game2 := Game{emptyBoard2, []byte{3, 5, 2, 5, 254}, []byte{4, 2, 6, 240}, false}
 
+	games := Games{game1, game2}
+	fmt.Println(games)
+
+	games.play()
+	fmt.Println(games[0].Board)
+	fmt.Println(games[1].Board)
+
+}
+
+func (games *Games) play() {
+	game := (*games)[0]
+	game.Board[4] = 'X'
 }
 
 type GameBoard []rune
