7eb409a0e5ca5a6fb5428c70a515ad22fd495f64
eyal <eyals@orbs.com>
2018-12-25 19:51:22 +0200

    "improved structs for battleship"

--- testing-serialization/newestBattleship/serializing.go
+++ testing-serialization/newestBattleship/serializing.go
@@ -1,39 +1,29 @@
 package main
 
 import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	var coo coordinate
-	coo.new(15, 3)
-	Guesses := guesses{coo, coo, coo}
-	guesses2 := append(Guesses, coordinate{4, 2})
-	player1 := []byte{3, 26, 240, 1}
-	player2 := []byte{84, 83, 84, 2}
-	hashedBoard1 := []byte{63, 72, 72, 7, 2}
-	hashedBoard2 := []byte{73, 72, 8, 2, 43}
-
-	var match game
-	match.new(player1, player2, hashedBoard1, hashedBoard2, 4, 0, Guesses, guesses2, false, coo)
-	matches := make(games)
-
-	fmt.Println(matches)
-
-	b, err := matches.MarshalJSON()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(b))
-
-	stuff := make(games)
-	err = stuff.UnmarshalJSON(b)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(stuff)
+	coo1 := coordinate{3, 5}
+	coo2 := coordinate{5, 2}
+	box1 := box{coo1, true}
+	box2 := box{coo2, false}
+	box3 := box{coo1, false}
+	box4 := box{coo2, true}
+	boxes := secretBoard{box1, box2, box3, box4}
+	fmt.Println("before being shuffled", boxes)
+	boxes.Shuffle()
+	fmt.Println("after being shuffled", boxes)
+	b := boxes.sha256("password")
+	fmt.Println(b)
 
 }
 
@@ -63,44 +53,63 @@ func (coo *coordinate) UnmarshalJSON(b []byte) (err error) {
 	return nil
 }
 
-type guesses []coordinate
+type guesses struct {
+	playerGuesses     []coordinate
+	opponentResponses []bool
+}
 
 func (guesses *guesses) MarshalJSON() (b []byte, err error) {
-	length := uint8(len(*guesses))
+	lengthGuesses := uint8(len(guesses.playerGuesses))
 	guessesMap := make(map[uint8][]byte)
-	guessesMap[0], err = json.Marshal(length)
-	if err != nil {
-		return []byte{}, err
-	}
-	for i := uint8(0); i < length; i++ {
-		guessesMap[i+1], err = (*guesses)[i].MarshalJSON()
+	for i := uint8(0); i < lengthGuesses; i++ {
+		b, err := guesses.playerGuesses[i].MarshalJSON()
 		if err != nil {
 			return []byte{}, err
 		}
+		guessesMap[i] = b
+	}
+	lengthResponses := uint8(len(guesses.opponentResponses))
+	for i := uint8(0); i < lengthResponses; i++ {
+		if guesses.opponentResponses[i] {
+			guessesMap[i] = append(guessesMap[i], byte(1))
+		} else {
+			guessesMap[i] = append(guessesMap[i], byte(0))
+		}
 	}
 	return json.Marshal(guessesMap)
+
 }
 func (guesses *guesses) UnmarshalJSON(b []byte) (err error) {
+
 	guessesMap := make(map[uint8][]byte)
 	err = json.Unmarshal(b, &guessesMap)
 	if err != nil {
 		return err
 	}
-	var length uint8
-	err = json.Unmarshal(guessesMap[0], &length)
-	if err != nil {
-		return err
-	}
-	for i := uint8(0); i < length; i++ {
-		var tempGuess coordinate
-		err = tempGuess.UnmarshalJSON(guessesMap[i+1])
+	for i := 0; i < len(guessesMap); i++ {
+		var guess coordinate
+		val := guessesMap[uint8(i)]
+		err = guess.UnmarshalJSON(val)
 		if err != nil {
-			return err
+			err = guess.UnmarshalJSON(val[:len(val)-1])
+			if err != nil {
+				return err
+			}
+			guesses.playerGuesses = append(guesses.playerGuesses, guess)
+			guesses.opponentResponses = append(guesses.opponentResponses, val[len(val)-1] == 1)
+		} else {
+			guesses.playerGuesses = append(guesses.playerGuesses, guess)
 		}
-		*guesses = append(*guesses, tempGuess)
 	}
 	return nil
-
+}
+func (guesses *guesses) exists(coo coordinate) (exists bool) {
+	for _, value := range guesses.playerGuesses {
+		if value == coo {
+			return true
+		}
+	}
+	return false
 }
 
 type game struct {
@@ -114,10 +123,14 @@ type game struct {
 	Player2Guesses    guesses
 	Player1Turn       bool
 	OpponentLastGuess coordinate
+
+	//1 means approved, 2 means not approved, 3 means didn't check
+	board1Approved uint8
+	board2Approved uint8
 }
 
 func (game *game) new(player1, player2, board1Hashed, board2Hashed []byte, player1Hits, player2Hits uint8,
-	player1Guesses, player2Guesses guesses, player1Turn bool, opponentLastGuess coordinate) {
+	player1Guesses, player2Guesses guesses, player1Turn bool, opponentLastGuess coordinate, board1Approved, board2Approved uint8) {
 	game.Player1 = player1
 	game.Player2 = player2
 	game.Board1Hashed = board1Hashed
@@ -128,6 +141,8 @@ func (game *game) new(player1, player2, board1Hashed, board2Hashed []byte, playe
 	game.Player2Guesses = player2Guesses
 	game.Player1Turn = player1Turn
 	game.OpponentLastGuess = opponentLastGuess
+	game.board1Approved = board1Approved
+	game.board2Approved = board2Approved
 }
 func (game *game) MarshalJSON() (b []byte, err error) {
 	gameMap := make(map[uint8][]byte)
@@ -154,7 +169,8 @@ func (game *game) MarshalJSON() (b []byte, err error) {
 	if err != nil {
 		return []byte{}, err
 	}
-
+	gameMap[10] = []byte{game.board1Approved}
+	gameMap[11] = []byte{game.board2Approved}
 	return json.Marshal(gameMap)
 
 }
@@ -190,6 +206,9 @@ func (game *game) UnmarshalJSON(b []byte) (err error) {
 	if err != nil {
 		return err
 	}
+	game.board1Approved = gameMap[10][0]
+	game.board2Approved = gameMap[11][0]
+
 	return nil
 
 }
@@ -222,4 +241,92 @@ func (games *games) UnmarshalJSON(b []byte) (err error) {
 		(*games)[i] = tempGame
 	}
 	return nil
+}
+
+type box struct {
+	coo  coordinate
+	ship bool
+}
+
+func (box *box) new(coo coordinate, ship bool) {
+	box.coo = coo
+	box.ship = ship
+}
+func (box *box) MarshalJSON() (b []byte, err error) {
+	boxMap := make(map[uint8][]byte)
+	b, err = box.coo.MarshalJSON()
+	if err != nil {
+		return []byte{}, err
+	}
+	boxMap[0] = b
+	if box.ship {
+		boxMap[1] = []byte{1}
+	} else {
+		boxMap[1] = []byte{0}
+	}
+	return json.Marshal(boxMap)
+}
+func (box *box) UnmarshalJSON(b []byte) (err error) {
+	boxMap := make(map[uint8][]byte)
+	err = json.Unmarshal(b, &boxMap)
+	if err != nil {
+		return err
+	}
+	coo := coordinate{}
+	err = coo.UnmarshalJSON(boxMap[0])
+	if err != nil {
+		return err
+	}
+	box.coo = coo
+	box.ship = bytes.Equal(boxMap[1], []byte{1})
+	return nil
+}
+
+type secretBoard []box
+
+func (boxes *secretBoard) MarshalJSON() (b []byte, err error) {
+	secretBoardMap := make(map[uint8][]byte)
+	for i, val := range *boxes {
+		b, err = val.MarshalJSON()
+		if err != nil {
+			return []byte{}, err
+		}
+		secretBoardMap[uint8(i)] = b
+	}
+	return json.Marshal(secretBoardMap)
+}
+func (boxes *secretBoard) UnmarshalJSON(b []byte) (err error) {
+	secretBoardMap := make(map[uint8][]byte)
+	err = json.Unmarshal(b, &secretBoardMap)
+	if err != nil {
+		return err
+	}
+	var box box
+	for i := 0; i < len(secretBoardMap); i++ {
+		err = box.UnmarshalJSON(secretBoardMap[uint8(i)])
+		if err != nil {
+			return err
+		}
+		*boxes = append(*boxes, box)
+	}
+	return nil
+}
+func (boxes *secretBoard) sha256(sk string) (sha []byte) {
+	h := hmac.New(sha256.New, []byte(sk))
+	b, err := boxes.MarshalJSON()
+	if err != nil {
+		log.Fatal(err)
+	}
+	h.Write(b)
+	return h.Sum(nil)
+
+}
+func (boxes *secretBoard) Shuffle() {
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	ret := make(secretBoard, len(*boxes))
+	perm := r.Perm(len(*boxes))
+	for i, randIndex := range perm {
+		ret[i] = (*boxes)[randIndex]
+	}
+	*boxes = ret
 }
